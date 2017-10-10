507f03121cb1f6065c1c3088d2d542355b66dc1b
Serra Allgood <sallgood@springbot.com>
2017-10-10 04:58:06 -0400

    Import CommonModule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { FilterListComponent } from './filter-list/filter-list.component';
 import { FilledArrayPipe } from './pipes/filled-array.pipe';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [FilterListComponent, FilledArrayPipe],
   exports: [FilterListComponent]
 })
