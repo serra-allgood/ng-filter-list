9070056f2b1bff69a46689002d162a09596a47ad
Serra Allgood <sallgood@springbot.com>
2017-10-09 20:34:40 -0400

    Attempt to fix aot compilation

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FilterListModule } from './filter-list/filter-list.module';
 
 @NgModule({
+  imports: [FilterListModule.forRoot()],
+  exports: [FilterListModule]
+})
+export class NgRootModule {}
+
+@NgModule({
   imports: [FilterListModule],
   exports: [FilterListModule]
 })
 export class NgFilterListModule {
   static forRoot(): ModuleWithProviders {
-    return { ngModule: NgFilterListModule };
+    return { ngModule: NgRootModule };
   }
 }
